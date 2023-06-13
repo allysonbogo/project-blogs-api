ae24df69b25fca98eeb6b01e28b312404155938c
Allyson Bogo <ab.bogo@gmail.com>
2023-06-13 17:32:59 -0300

    requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    'PostCategory',
+    {
+      postId: { type: DataTypes.INTEGER, primaryKey: true },
+      categoryId: { type: DataTypes.INTEGER, primaryKey: true },
+    },
+    {
+      timestamps: false,
+      tableName: 'posts_categories',
+      underscored: true,
+    },
+  );
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost,
+      { through: PostCategory, foreignKey: 'categoryId', otherKey: 'postId' });
+
+    models.BlogPost.belongsToMany(models.Category,
+      { through: PostCategory, foreignKey: 'postId', otherKey: 'categoryId' });
+  };
+
+  return PostCategory;
+};
